826be35627c3660a33df22203aca8734513ab02f
Mark Lister <toomuchspam@nowhere>
2015-02-28 12:50:47 +0200

    Add tests from marklister's commit 013afd0becf8171c05206e227b2dbe72d98c6344
    Range parsing too eager, encode sharp

--- src/test/scala/spray/boilerplate/TemplateParserSpecs.scala
+++ src/test/scala/spray/boilerplate/TemplateParserSpecs.scala
@@ -28,9 +28,19 @@ class TemplateParserSpecs extends Specification {
     "custom range" in {
       parse("[0..14#T1#]") === Expand(LiteralString("T") ~ Offset(1), range = Range(start = Some(0), end = Some(14)))
     }
+    "not a range" in {
+      parse("[ abc #T ]") === LiteralString("[ abc #T ]")
+    }
+    "sharp" in {
+      parse("""\#""") === LiteralString("#")
+    }
     "prefix, custom range, and separator" in {
       parse("abc[0..14#T1#\n]") ===
         LiteralString("abc") ~ Expand(LiteralString("T") ~ Offset(1), separator = "\n", range = Range(start = Some(0), end = Some(14)))
+    }
+    "Sharp is parsed as literal" in {
+      parse("""def apply[ T : |¬|[Product]\#λ](s : T *):CollSeq1[T] = apply(s.map(Tuple1(_)): _*)""") ===
+        LiteralString("""def apply[ T : |¬|[Product]#λ](s : T *):CollSeq1[T] = apply(s.map(Tuple1(_)): _*)""")
     }
   }
 }
